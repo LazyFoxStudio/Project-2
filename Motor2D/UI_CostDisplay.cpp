9e16fe527b95e2772612c176884ae0cbb92a4641
joseppi <josep.pi.serra@gmail.com>
2018-04-08 18:59:35 +0200

    Added cost display

---
+++ Motor2D/UI_CostDisplay.cpp
@@ -0,0 +1,17 @@
+#include "UI_CostDisplay.h"
+#include "UI_Text.h"
+
+
+
+CostDisplay::CostDisplay(std::string entityname, int cost, resourceType resource)
+{
+	//Text* ret = new Text(text, x, y, (*font), color, callback);
+	
+};
+
+
+CostDisplay::~CostDisplay()
+{
+	
+};
+
