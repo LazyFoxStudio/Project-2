06aeb2276f8374645fed1251c26a090d096f0fcc
Adria-F <adriaferrer2898@gmail.com>
2018-06-05 17:34:52 +0200

    fixed #196 & updated heroes icons

--- Motor2D/j1Gui.cpp
+++ Motor2D/j1Gui.cpp
@@ -117,7 +117,7 @@ bool j1Gui::PreUpdate()
 			{
 				ret = element->callback->OnUIEvent(element, MOUSE_LEFT_CLICK);
 			}
-			if (element->element_type == BUTTON)
+			if (element->element_type == BUTTON && !element->isLocked())
 				App->audio->PlayFx(SFX_BUTTON_CLICKED);
 
 			if (element->dragable)
